ecc1b04806315bfe5e09f14a0c75e3cc824b636d
jorgeoliveira <ptjoliveira@gmail.com>
2023-03-06 17:16:01 +0000

    new

--- _app/immutable/chunks/0-801fb482.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_layout-f57c96be.js";import{default as t}from"../components/pages/_layout.svelte-3cec7bf2.js";export{t as component,r as universal};
--- _app/immutable/chunks/10-71473c4f.js
+++ _app/immutable/chunks/10-71473c4f.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-287d30ac.js";import{default as t}from"../components/pages/pop-news/_slug_/_page.svelte-e3f4ff85.js";export{t as component,r as universal};
+import{_ as r}from"./_page-c692e71b.js";import{default as t}from"../components/pages/pop-news/_slug_/_page.svelte-e3f4ff85.js";export{t as component,r as universal};
